49b63e3265ac40c174813b70350b8fab95cd185c
basilmuhmd <basilmuhammed194@gmail.com>
2024-03-15 12:50:15 +0530

    minor bug fix

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -46,89 +46,93 @@ class HomePage extends ConsumerWidget {
               ],
             ),
             const SizedBox(height: 32),
-            ref.watch(productProvider).when(
-                  data: (data) {
-                    if (data.docs.isEmpty) {
-                      return const Center(
-                        child: Text(
-                            "No products found in database, Add a product"),
-                      );
-                    }
-                    return ListView.builder(
-                      itemCount: data.docs.length,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        final product = data.docs[index].data();
-                        return Card(
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              child: Text("${index + 1}"),
-                            ),
-                            title: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("Name: ${product.name}"),
-                                Text("SubName: ${product.subName}"),
-                                Text("Rate: ${product.rate}"),
-                              ],
-                            ),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                ButtonWidget(
-                                  btnText: 'Update',
-                                  btnColor: Colors.amber,
-                                  width: 50,
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => AddProduct(
-                                            isEdit: true,
-                                            name: product.name,
-                                            subName: product.subName,
-                                            rate: product.rate,
-                                            id: product.id,
-                                          ),
-                                        ));
-                                  },
+            ref.watch(productProvider).isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ref.watch(productProvider).when(
+                      data: (data) {
+                        if (data.docs.isEmpty) {
+                          return const Center(
+                            child: Text(
+                                "No products found in database, Add a product"),
+                          );
+                        }
+                        return ListView.builder(
+                          itemCount: data.docs.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            final product = data.docs[index].data();
+                            return Card(
+                              child: ListTile(
+                                leading: CircleAvatar(
+                                  child: Text("${index + 1}"),
                                 ),
-                                const SizedBox(width: 8),
-                                ButtonWidget(
-                                  btnText: 'Delete',
-                                  btnColor: Colors.red,
-                                  width: 50,
-                                  onTap: () {
-                                    ref
-                                        .read(productProvider.notifier)
-                                        .removeProduct(product.id);
-                                  },
+                                title: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Name: ${product.name}"),
+                                    Text("SubName: ${product.subName}"),
+                                    Text("Rate: ${product.rate}"),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    ButtonWidget(
+                                      btnText: 'Update',
+                                      btnColor: Colors.amber,
+                                      width: 50,
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => AddProduct(
+                                                isEdit: true,
+                                                name: product.name,
+                                                subName: product.subName,
+                                                rate: product.rate,
+                                                id: product.id,
+                                              ),
+                                            ));
+                                      },
+                                    ),
+                                    const SizedBox(width: 8),
+                                    ButtonWidget(
+                                      btnText: 'Delete',
+                                      btnColor: Colors.red,
+                                      width: 50,
+                                      onTap: () {
+                                        ref
+                                            .read(productProvider.notifier)
+                                            .removeProduct(product.id);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
                         );
                       },
-                    );
-                  },
-                  error: (error, stackTrace) => Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                          "${(error is FirebaseException) ? error.message : "An unknown error occured, please retry."},"),
-                      IconButton(
-                        onPressed: () {
-                          ref.invalidate(productProvider);
-                        },
-                        icon: const Icon(Icons.refresh),
+                      error: (error, stackTrace) => Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                              "${(error is FirebaseException) ? error.message : "An unknown error occured, please retry."},"),
+                          IconButton(
+                            onPressed: () {
+                              ref.invalidate(productProvider);
+                            },
+                            icon: const Icon(Icons.refresh),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  loading: () => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                )
+                      loading: () => const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    )
           ],
         ));
   }
